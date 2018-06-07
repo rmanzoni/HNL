59f647b5d70e5306f375c237e1e65e59e3f097af
Riccardo Manzoni <riccardo.manzoni@cern.ch>
2018-06-07 16:24:35 +0200

    up the skims up up!

--- skim/skim_by_hlt_singlemu_cfg.py
+++ skim/skim_by_hlt_singlemu_cfg.py
@@ -9,19 +9,19 @@ process = cms.Process('HNLSKIM')
 process.source = cms.Source (
     'PoolSource',
     fileNames=cms.untracked.vstring(
-        '/store/data/Run2016H/SingleMuon/MINIAOD/18Apr2017-v1/90000/46483D6E-A33E-E711-B0F3-3417EBE644B3.root',
+#         '/store/data/Run2016H/SingleMuon/MINIAOD/18Apr2017-v1/90000/46483D6E-A33E-E711-B0F3-3417EBE644B3.root',
+        '/store/data/Run2017D/SingleMuon/MINIAOD/31Mar2018-v1/90000/D42C6863-CB37-E811-B651-20CF3027A5BB.root'
     ),
-#     secondaryFileNames = cms.untracked.vstring(
-#         '/store/mc/RunIISummer16DR80Premix/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext2-v1/110002/062AB3F9-48F1-E611-B7BC-02163E0137A0.root',
-#         '/store/mc/RunIISummer16DR80Premix/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext2-v1/110002/124AC56C-42F1-E611-91DB-02163E019D4A.root',
-#         '/store/mc/RunIISummer16DR80Premix/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext2-v1/110002/2029FDDB-44F1-E611-91E2-02163E01436E.root',
-#         '/store/mc/RunIISummer16DR80Premix/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext2-v1/110002/22AAE483-46F1-E611-901C-02163E019BD7.root',
-#         '/store/mc/RunIISummer16DR80Premix/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext2-v1/110002/30D98C18-40F1-E611-A97C-02163E019B82.root',
-#         '/store/mc/RunIISummer16DR80Premix/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext2-v1/110002/8475F869-42F1-E611-9CBC-02163E01A495.root',
-#         '/store/mc/RunIISummer16DR80Premix/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext2-v1/110002/8A2C77FF-48F1-E611-859A-02163E014447.root',
-#         '/store/mc/RunIISummer16DR80Premix/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext2-v1/110002/A413BD73-42F1-E611-8D69-02163E014294.root',
-#         '/store/mc/RunIISummer16DR80Premix/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext2-v1/110002/B0DA89D6-44F1-E611-A240-02163E019DDC.root',
-#   )
+    secondaryFileNames = cms.untracked.vstring(
+        '/store/data/Run2017D/SingleMuon/AOD/17Nov2017-v1/50000/84E6A8AA-7DE4-E711-91C4-0026B92779B0.root',
+        '/store/data/Run2017D/SingleMuon/AOD/17Nov2017-v1/50000/16178FA7-7DE4-E711-BE46-BC305B390AA7.root',
+        '/store/data/Run2017D/SingleMuon/AOD/17Nov2017-v1/60000/00BF93B0-88DA-E711-8012-7845C4FC374C.root',
+        '/store/data/Run2017D/SingleMuon/AOD/17Nov2017-v1/50000/B42116D2-78E4-E711-87FF-0023AEEEB79C.root',
+        '/store/data/Run2017D/SingleMuon/AOD/17Nov2017-v1/60000/145EE914-B7DA-E711-B26B-180373FFCE1E.root',
+        '/store/data/Run2017D/SingleMuon/AOD/17Nov2017-v1/50000/A21241D2-78E4-E711-BA6A-0023AEEEB79C.root',
+        '/store/data/Run2017D/SingleMuon/AOD/17Nov2017-v1/60000/F8B0CF58-ABDA-E711-827D-008CFAFBE5CE.root',
+        '/store/data/Run2017D/SingleMuon/AOD/17Nov2017-v1/60000/A29DFB76-C0DA-E711-82FE-3417EBE6457C.root',
+  )
 )
 
 # limit the number of events to be processed
@@ -42,7 +42,8 @@ process.hltSkim = cms.Path( process.triggerSelection )
 
 # talk to output module
 process.out = cms.OutputModule('PoolOutputModule',
-    fileName = cms.untracked.string('miniAOD_singlemu_skim.root'),
+#     fileName = cms.untracked.string('miniAOD_singlemu_skim.root'),
+    fileName = cms.untracked.string('miniAOD_skim.root'),
     SelectEvents = cms.untracked.PSet(
         SelectEvents = cms.vstring('hltSkim',)
     ),

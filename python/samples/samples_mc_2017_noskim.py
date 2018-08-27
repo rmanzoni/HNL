7132ae765c05ccd51b4483ec379000c62ad4f85d
Riccardo Manzoni <riccardo.manzoni@cern.ch>
2018-08-27 15:20:19 +0200

    merge from david

---
+++ python/samples/samples_mc_2017_noskim.py
@@ -0,0 +1,235 @@
+import PhysicsTools.HeppyCore.framework.config as cfg
+import os
+
+#####COMPONENT CREATOR
+
+from CMGTools.RootTools.samples.ComponentCreator import ComponentCreator
+
+creator = ComponentCreator()
+
+
+ZZZ = creator.makeMCComponent(
+    name    = 'ZZZ', 
+    dataset = '/ZZZ_TuneCP5_13TeV-amcatnlo-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM',
+    user    = 'CMS', 
+    pattern = '.*root', 
+    xSec    = 0.01398, 
+    useAAA  = True
+)
+
+ZZZ .nGenEvents = ZZZ.dataset_entries 
+
+
+WZZ = creator.makeMCComponent(
+    name    = 'WZZ', 
+    dataset = '/WZZ_TuneCP5_13TeV-amcatnlo-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM',
+    user    = 'CMS', 
+    pattern = '.*root', 
+    xSec    = 0.05565, 
+    useAAA  = True
+)
+
+WZZ .nGenEvents = WZZ.dataset_entries 
+
+
+WWZ = creator.makeMCComponent(
+    name    = 'WWZ', 
+    dataset = '/WWZ_4F_TuneCP5_13TeV-amcatnlo-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM',
+    user    = 'CMS', 
+    pattern = '.*root', 
+    xSec    = 0.1651, 
+    useAAA  = True
+)
+
+WWZ .nGenEvents = WWZ.dataset_entries 
+
+
+WWW = creator.makeMCComponent(
+    name    = 'WWW', 
+    dataset = '/WWW_4F_TuneCP5_13TeV-amcatnlo-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM',
+    user    = 'CMS', 
+    pattern = '.*root', 
+    xSec    = 0.2086, 
+    useAAA  = True
+)
+
+WWW .nGenEvents = WWW.dataset_entries 
+
+
+WWTo2L2Nu = creator.makeMCComponent(
+    name    = 'WWTo2L2Nu', 
+    dataset = '/WWTo2L2Nu_NNPDF31_TuneCP5Up_13TeV-powheg-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v2/MINIAODSIM',
+    user    = 'CMS', 
+    pattern = '.*root', 
+    xSec    = 0.1729, 
+    useAAA  = True
+)
+
+WWTo2L2Nu .nGenEvents = WWTo2L2Nu.dataset_entries 
+
+
+WGGJets = creator.makeMCComponent(
+    name    = 'WGGJets', 
+    dataset = '/WGGJets_TuneCP5_13TeV_madgraphMLM_pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM',
+    user    = 'CMS', 
+    pattern = '.*root', 
+    xSec    = 0.03711, 
+    useAAA  = True
+)
+
+WGGJets .nGenEvents = WGGJets.dataset_entries 
+
+
+TTWJetsToLNu = creator.makeMCComponent(
+    name    = 'TTWJetsToLNu', 
+    dataset = '/TTWJetsToLNu_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM',
+    user    = 'CMS', 
+    pattern = '.*root', 
+    xSec    = 0.2043, 
+    useAAA  = True
+)
+
+TTWJetsToLNu .nGenEvents = TTWJetsToLNu.dataset_entries 
+
+
+TTZToLLNuNu = creator.makeMCComponent(
+    name    = 'TTZToLLNuNu', 
+    dataset = '/TTZToLLNuNu_M-10_TuneCP5_13TeV-amcatnlo-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM',
+    user    = 'CMS', 
+    pattern = '.*root', 
+    xSec    = 0.2529, 
+    useAAA  = True
+)
+
+TTZToLLNuNu .nGenEvents = TTZToLLNuNu.dataset_entries 
+
+
+TTZToLL = creator.makeMCComponent(
+    name    = 'TTZToLL', 
+    dataset = '/TTZToLL_M-1to10_TuneCP5_13TeV-amcatnlo-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM',
+    user    = 'CMS', 
+    pattern = '.*root', 
+    # xSec    = #FIXME 
+    useAAA  = True
+)
+
+TTZToLL .nGenEvents = TTZToLL.dataset_entries 
+
+
+ST_s_channel_4f_leptonDecays = creator.makeMCComponent(
+    name    = 'ST_s-channel_4f_leptonDecyas', 
+    dataset = '/ST_s-channel_4f_leptonDecays_TuneCP5_13TeV-amcatnlo-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM',
+    user    = 'CMS', 
+    pattern = '.*root', 
+    xSec    = 3.68, 
+    useAAA  = True
+)
+
+ST_s_channel_4f_leptonDecays .nGenEvents = ST_s_channel_4f_leptonDecays.dataset_entries 
+
+
+ST_t_channel_antitop_4f_inclusiveDecays = creator.makeMCComponent(
+    name    = 'ST_t-channel_antitop_4f_inclusiveDecays', 
+    dataset = '/ST_t-channel_antitop_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM',
+    user    = 'CMS', 
+    pattern = '.*root', 
+    xSec    = 26.23, 
+    useAAA  = True
+)
+
+ST_t_channel_antitop_4f_inclusiveDecays .nGenEvents = ST_t_channel_antitop_4f_inclusiveDecays .dataset_entries 
+
+
+ST_t_channel_top_4f_inclusiveDecays = creator.makeMCComponent(
+    name    = 'ST_t-channel_top_4f_inclusiveDecays', 
+    dataset = '/ST_t-channel_top_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_new_pmx_94X_mc2017_realistic_v14-v1/MINIAODSIM',
+    user    = 'CMS', 
+    pattern = '.*root', 
+    xSec    = 44.07, 
+    useAAA  = True
+)
+
+ST_t_channel_top_4f_inclusiveDecays .nGenEvents = ST_t_channel_top_4f_inclusiveDecays.dataset_entries 
+
+
+ST_tW_antitop_5f_inclusiveDecays = creator.makeMCComponent(
+    name    = 'ST_tW_antitop_5f_inclusiveDecays', 
+    dataset = '/ST_tW_antitop_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM',
+    user    = 'CMS', 
+    pattern = '.*root', 
+    xSec    = 35.6, 
+    useAAA  = True
+)
+
+ST_tW_antitop_5f_inclusiveDecays .nGenEvents = ST_tW_antitop_5f_inclusiveDecays.dataset_entries 
+
+
+ST_tW_top_5f_inclusiveDecays = creator.makeMCComponent(
+    name    = 'ST_tW_top_5f_inclusiveDecays', 
+    dataset = '/ST_tW_top_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM',
+    user    = 'CMS', 
+    pattern = '.*root', 
+    xSec    = 35.6, 
+    useAAA  = True
+)
+
+ST_tW_top_5f_inclusiveDecays .nGenEvents = ST_tW_top_5f_inclusiveDecays.dataset_entries 
+
+
+
+# LINK for crosssections: https://twiki.cern.ch/twiki/bin/viewauth/CMS/SummaryTable1G25ns
+
+##########################################################################################
+# assign to each sample its own PU profile file. For 2017 it is important to do it per-sample
+##########################################################################################
+ZZZ  .puFileMC = os.environ['CMSSW_BASE'] + '/src/CMGTools/HNL/data/pileup/pileup_ZZZ.root' # derived manually 
+WZZ  .puFileMC = os.environ['CMSSW_BASE'] + '/src/CMGTools/HNL/data/pileup/pileup_WZZ.root' # derived manually 
+WWZ  .puFileMC = os.environ['CMSSW_BASE'] + '/src/CMGTools/HNL/data/pileup/pileup_WWZ.root' # derived manually 
+WWW  .puFileMC = os.environ['CMSSW_BASE'] + '/src/CMGTools/HNL/data/pileup/pileup_WWW.root' # derived manually 
+WWTo2L2Nu  .puFileMC = os.environ['CMSSW_BASE'] + '/src/CMGTools/HNL/data/pileup/pileup_WWTo2L2Nu.root' # derived manually 
+WGGJets  .puFileMC = os.environ['CMSSW_BASE'] + '/src/CMGTools/HNL/data/pileup/pileup_WGGJets.root' # derived manually 
+TTWJetsToLNu  .puFileMC = os.environ['CMSSW_BASE'] + '/src/CMGTools/HNL/data/pileup/pileup_TTWJetsToLNu.root' # derived manually 
+TTZToLLNuNu  .puFileMC = os.environ['CMSSW_BASE'] + '/src/CMGTools/HNL/data/pileup/pileup_TTZToLLNuNu.root' # derived manually 
+TTZToLL  .puFileMC = os.environ['CMSSW_BASE'] + '/src/CMGTools/HNL/data/pileup/pileup_TTZToLL.root' # derived manually 
+ST_s_channel_4f_leptonDecays  .puFileMC = os.environ['CMSSW_BASE'] + '/src/CMGTools/HNL/data/pileup/pileup_ST_s_channel_4f_leptonDecays.root' # derived manually 
+ST_t_channel_antitop_4f_inclusiveDecays  .puFileMC = os.environ['CMSSW_BASE'] + '/src/CMGTools/HNL/data/pileup/pileup_ST_t_channel_antitop_4f_inclusiveDecays.root' # derived manually 
+ST_t_channel_top_4f_inclusiveDecays  .puFileMC = os.environ['CMSSW_BASE'] + '/src/CMGTools/HNL/data/pileup/pileup_ST_t_channel_top_4f_inclusiveDecays.root' # derived manually 
+ST_tW_antitop_5f_inclusiveDecays  .puFileMC = os.environ['CMSSW_BASE'] + '/src/CMGTools/HNL/data/pileup/pileup_ST_tW_antitop_5f_inclusiveDecays.root' # derived manually 
+ST_tW_top_5f_inclusiveDecays  .puFileMC = os.environ['CMSSW_BASE'] + '/src/CMGTools/HNL/data/pileup/pileup_ST_tW_top_5f_inclusiveDecays.root' # derived manually 
+
+
+##########################################################################################
+hnl_bkg_noskim = [
+    ZZZ, 
+    WZZ, 
+    WWZ, 
+    WWW, 
+    WWTo2L2Nu, 
+    WGGJets, 
+    TWJetsToLNu, 
+    TTZToLLNuNu, 
+    TTZToLL, 
+    ST_s_channel_4f_leptonDecays, 
+    ST_t_channel_antitop_4f_inclusiveDecays, 
+    ST_t_channel_top_4f_inclusiveDecays, 
+    ST_tW_antitop_5f_inclusiveDecays, 
+    ST_tW_top_5f_inclusiveDecays 
+] 
+
+
+##########################################################################################
+#  data PileUp profile
+##########################################################################################
+# https://twiki.cern.ch/twiki/bin/view/CMS/PileupJSONFileforData#Pileup_JSON_Files_For_Run_II
+# have a loo at this too, the mb cross section might be off https://hypernews.cern.ch/HyperNews/CMS/get/luminosity/755.html
+'''
+pileupCalc.py -i /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/Final/Cert_294927-306462_13TeV_PromptReco_Collisions17_JSON.txt\
+              --inputLumiJSON /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/PileUp/pileup_latest.txt\
+              --calcMode true --minBiasXsec 69200 --maxPileupBin 200 --numPileupBins 200  pileup_data_golden_json_2017.root
+'''
+for ibkg in hnl_bkg_noskim:
+    ibkg.puFileData = os.environ['CMSSW_BASE'] + '/src/CMGTools/HNL/data/pileup/pileup_data_golden_json_2017.root'
+
+
+
+

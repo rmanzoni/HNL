eef3d25ac351d54f0a76c48704937727457d9550
Riccardo Manzoni <riccardo.manzoni@cern.ch>
2018-11-17 18:14:30 +0100

    more work on B field, no cigar yet

--- src/LinkDef.h
+++ src/LinkDef.h
@@ -1,18 +1,32 @@
 #ifdef __MAKECINT__
-#pragma link C++ defined_in "MagneticField/Engine/interface/MagneticField.h";
-#pragma link C++ defined_in "MagneticField/Layers/interface/MagESector.h"
-#pragma link C++ defined_in "MagneticField/Layers/src/MagBinFinders.h";
-#pragma link C++ defined_in "MagneticField/Records/interface/IdealMagneticFieldRecord.h";
-#pragma link C++ defined_in "MagneticField/VolumeGeometry/interface/MagVolume.h";
-#pragma link C++ defined_in "MagneticField/VolumeBasedEngine/interface/VolumeBasedMagneticField.h";
-#pragma link C++ defined_in "MagneticField/VolumeBasedEngine/interface/MagGeometry.h";
-#pragma link C++ defined_in "FWCore/Framework/interface/EventSetup.h";
-#pragma link C++ defined_in "FWCore/Framework/interface/EventSetupRecord.h";
-#pragma link C++ defined_in "FWCore/Framework/interface/EventSetupRecordImplementation.h";
-#pragma link C++ defined_in "DataFormats/FWLite/interface/ESHandle.h";
-#pragma link C++ defined_in "MagneticField/UniformEngine/interface/UniformMagneticField.h";
-#pragma link C++ class std::atomic<char>+;
-#pragma link C++ class __atomic_base<char>+;
+// #pragma link C++ defined_in "MagneticField/Engine/interface/MagneticField.h";
+// #pragma link C++ defined_in "MagneticField/Layers/interface/MagESector.h";
+// #pragma link C++ defined_in "MagneticField/Layers/src/MagBinFinders.h";
+// #pragma link C++ defined_in "MagneticField/Records/interface/IdealMagneticFieldRecord.h";
+// #pragma link C++ defined_in "MagneticField/VolumeGeometry/interface/MagVolume.h";
+// #pragma link C++ defined_in "MagneticField/VolumeBasedEngine/interface/VolumeBasedMagneticField.h";
+// #pragma link C++ defined_in "MagneticField/VolumeBasedEngine/interface/MagGeometry.h";
+// #pragma link C++ defined_in "FWCore/Framework/interface/EventSetup.h";
+// #pragma link C++ defined_in "FWCore/Framework/interface/EventSetupRecord.h";
+// #pragma link C++ defined_in "FWCore/Framework/interface/EventSetupRecordImplementation.h";
+// #pragma link C++ defined_in "DataFormats/FWLite/interface/ESHandle.h";
+// #pragma link C++ defined_in "MagneticField/UniformEngine/interface/UniformMagneticField.h";
+// #pragma link C++ defined_in "MagneticField/VolumeGeometry/interface/VolumeSide.h";
+// #pragma link C++ defined_in "DataFormats/GeometrySurface/interface/Surface.h";
+// #pragma link C++ class std::atomic<char>+;
+// #pragma link C++ class __atomic_base<char>+;
+// #pragma link C++ class __atomic_base<const MagVolume*>+;
+// #pragma link C++ class VolumeBasedMagneticField+;
+
+
+
+
+
+
+
+
+
+
 // #pragma link C++ class MagVolume*+;
 // #pragma link C++ class atomic<MagVolume*>+ alksjdalksjdalskjd;
 // #pragma link C++ jhgjhgjhg class atomic<const MagVolume*>+;

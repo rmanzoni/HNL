a7914bd352776ee6f864f0ef8c34e0b0b8bcb84f
Vinzenz <v.stampf@me.com>
2019-01-24 13:20:59 +0100

    including two gen flags to tell whether electrons or muons come from prompt or hardprocess taus

--- cfg/hn3l_mee_cfg.py
+++ cfg/hn3l_mee_cfg.py
@@ -14,7 +14,7 @@ from CMGTools.HNL.samples.samples_mc_2017 import TTJets_amcat
 from CMGTools.HNL.samples.samples_mc_2017_noskim import DYBB, DYJetsToLL_M10to50, DYJetsToLL_M50, DYJetsToLL_M10to50_ext, DYJetsToLL_M50_ext
 # samples = [Single_ele_2017B,Single_ele_2017C,Single_ele_2017D,Single_ele_2017E,Single_ele_2017F]
 samples = [DYBB, DYJetsToLL_M10to50, DYJetsToLL_M50, DYJetsToLL_M10to50_ext, DYJetsToLL_M50_ext]
-samples = [DYJetsToLL_M50, DYJetsToLL_M50_ext]
+samples = [DYBB, DYJetsToLL_M10to50, DYJetsToLL_M50, DYJetsToLL_M50_ext]
 
 # edit the lines here to specify your ntuple production mode 
 production         = True # state whether you're running production mode or not
--- cfg/hn3l_mmm_cfg.py
+++ cfg/hn3l_mmm_cfg.py
@@ -16,9 +16,15 @@ from CMGTools.HNL.samples.samples_data_2017_noskim import Single_mu_2017, Single
 samples = [Single_mu_2017B]
 # samples = [HN3L_M_2_V_0p0141421356237_mu_massiveAndCKM_LO]
 
+from CMGTools.HNL.samples.samples_mc_2017 import TTJets_amcat
+from CMGTools.HNL.samples.samples_mc_2017_noskim import DYBB, DYJetsToLL_M10to50, DYJetsToLL_M50, DYJetsToLL_M10to50_ext, DYJetsToLL_M50_ext
+# samples = [Single_ele_2017B,Single_ele_2017C,Single_ele_2017D,Single_ele_2017E,Single_ele_2017F]
+samples = [DYBB, DYJetsToLL_M10to50, DYJetsToLL_M50, DYJetsToLL_M10to50_ext, DYJetsToLL_M50_ext]
+samples = [DYBB, DYJetsToLL_M10to50, DYJetsToLL_M50, DYJetsToLL_M50_ext]
+
 # edit the lines here to specify your ntuple production mode 
 production         = True # state whether you're running production mode or not
-isData             = True
+isData             = False
 isSignal           = False
 promptLeptonType   = "mu" # choose from 'ele', 'mu'
 L1L2LeptonType     = "mm"  # choose from 'ee', 'mm', 'em'

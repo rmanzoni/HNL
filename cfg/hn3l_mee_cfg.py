79e4e12b4c9d5554fb7e24ada5ae35eb9c762330
Dehua Zhu <dehua@zhu.icu>
2019-04-09 20:26:05 +0200

    trackin all cfg files

---
+++ cfg/hn3l_mee_cfg.py
@@ -0,0 +1,18 @@
+from CMGTools.HNL.hn3l_cfg import *
+
+# specify the samples considered
+from CMGTools.HNL.samples.samples_data_2017_noskim import Single_mu_2017, Single_mu_2017B, Single_mu_2017C, Single_mu_2017D, Single_mu_2017E, Single_mu_2017F
+from CMGTools.HNL.samples.samples_mc_2017_noskim import TTJets, WJetsToLNu, WJetsToLNu_ext, DYBB, DYJetsToLL_M10to50, DYJetsToLL_M50, DYJetsToLL_M50_ext, WW, WZ, ZZ 
+
+# samples = [TTJets, WJetsToLNu, WJetsToLNu_ext, DYBB, DYJetsToLL_M10to50, DYJetsToLL_M50, DYJetsToLL_M50_ext, WW, WZ, ZZ]
+samples = [Single_mu_2017B, Single_mu_2017C, Single_mu_2017D, Single_mu_2017E, Single_mu_2017F]
+
+# edit the lines here to specify your ntuple production mode 
+production         = True # state whether you're running production mode or not
+isData             = False
+isSignal           = False
+promptLeptonType   = "mu" # choose from 'ele', 'mu'
+L1L2LeptonType     = "ee"  # choose from 'ee', 'mm', 'em'
+
+# this calls the master cfg file with the proper settings
+config = generateKeyConfigs(samples,production, promptLeptonType, L1L2LeptonType, isData = isData, isSignal = isSignal)

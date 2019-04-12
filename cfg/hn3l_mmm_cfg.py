610972d2a28f248f38d520c62d822ca7b178f6c5
Dehua Zhu <dehua@zhu.icu>
2019-04-12 10:30:01 +0200

    changing back the split factors for mainline ques (10h) and adapting the cfg file for production

--- cfg/hn3l_mmm_cfg.py
+++ cfg/hn3l_mmm_cfg.py
@@ -1,18 +1,16 @@
 from CMGTools.HNL.hn3l_cfg import *
 
 # specify the samples considered
-from CMGTools.HNL.samples.localsignal import Single_mu_2017B
-# from CMGTools.HNL.samples.samples_data_2017_noskim import Single_mu_2017, Single_mu_2017B, Single_mu_2017C, Single_mu_2017D, Single_mu_2017E, Single_mu_2017F
+from CMGTools.HNL.samples.samples_data_2017_noskim import Single_mu_2017, Single_mu_2017B, Single_mu_2017C, Single_mu_2017D, Single_mu_2017E, Single_mu_2017F
 from CMGTools.HNL.samples.samples_mc_2017_noskim import TTJets, WJetsToLNu, WJetsToLNu_ext, DYBB, DYJetsToLL_M10to50, DYJetsToLL_M50, DYJetsToLL_M50_ext, WW, WZ, ZZ 
 
-# samples = [TTJets, WJetsToLNu, WJetsToLNu_ext, DYBB, DYJetsToLL_M10to50, DYJetsToLL_M50, DYJetsToLL_M50_ext, WW, WZ, ZZ]
+samples = [TTJets, WJetsToLNu, WJetsToLNu_ext, DYBB, DYJetsToLL_M10to50, DYJetsToLL_M50, DYJetsToLL_M50_ext, WW, WZ, ZZ]
 # samples = [Single_mu_2017B, Single_mu_2017C, Single_mu_2017D, Single_mu_2017E, Single_mu_2017F]
-samples  = [Single_mu_2017B]
 
 
 # edit the lines here to specify your ntuple production mode production         = False # state whether you're running production mode or not
-production         = False # state whether you're running production mode or not
-isData             = True
+production         = True # state whether you're running production mode or not
+isData             = False
 isSignal           = False
 promptLeptonType   = "mu" # choose from 'ele', 'mu'
 L1L2LeptonType     = "mm"  # choose from 'ee', 'mm', 'em'
